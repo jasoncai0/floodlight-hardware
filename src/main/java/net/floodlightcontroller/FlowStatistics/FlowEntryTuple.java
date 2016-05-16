fe7b8e8be2b5ccb91bad69c327b38583ba8d4fc8
zhenshengcai <zhenshengcai@gmail.com>
2016-05-16 19:59:13 +0800

    floodlight

--- src/main/java/net/floodlightcontroller/FlowStatistics/FlowEntryTuple.java
+++ src/main/java/net/floodlightcontroller/FlowStatistics/FlowEntryTuple.java
@@ -1,10 +1,10 @@
-package net.floodlightcontroller.FlowStatistics;
+package net.floodlightcontroller.flowstatistics;
 
-import org.projectfloodlight.openflow.types.U128;
+
 
 /**
  * Created by zhensheng on 2016/5/16.
  */
 public class FlowEntryTuple {
-    U128
+
 }
